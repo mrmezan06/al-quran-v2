3af956b12ba1edf69c991ac898478bb6d04dfead
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-10-27 06:32:30 +0600

    update server BASE URL

--- client/src/constants/BASE_URL.js
+++ client/src/constants/BASE_URL.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:5000/api/v2';
+// export const BASE_URL = 'http://localhost:5000/api/v2';
+export const BASE_URL = 'https://cyan-exuberant-hedgehog.cyclic.app/api/v2';
 // export const BASE_URL = 'https://quran-api-n1hq.onrender.com';
